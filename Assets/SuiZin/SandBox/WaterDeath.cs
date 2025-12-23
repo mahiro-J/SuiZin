205b8b3e41b8976cf4853d2a0312f94dda274862
MahiroThinkPad <mahiromugi@gmail.com>
2025-12-23 14:35:14 +0900

    seaに触れたらdeath

--- Assets/SuiZin/SandBox/WaterDeath.cs
+++ Assets/SuiZin/SandBox/WaterDeath.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using R3;
+using R3.Triggers;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody))]
 
 public class WaterDeath : MonoBehaviour
 {
+     [SerializeField]private Rigidbody rb;
+     [SerializeField]private BoxCollider seaCollider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Button button;
     void Start()
     {
-        
+        seaCollider.OnTriggerEnterAsObservable()
+            .Subscribe(_ => Death())
+            .AddTo(this);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    void Death()
+    {
+        Destroy(gameObject);
     }
 }
