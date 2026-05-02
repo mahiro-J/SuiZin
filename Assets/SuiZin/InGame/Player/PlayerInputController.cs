093c8ddf86b96970eb4bef732a336d26a31017b8
MahiroThinkPad <mahiromugi@gmail.com>
2026-05-02 18:30:37 +0900

    プレイヤーがモーダル表示中に移動できるようにした

--- Assets/SuiZin/InGame/Player/PlayerInputController.cs
+++ Assets/SuiZin/InGame/Player/PlayerInputController.cs
@@ -1,7 +1,5 @@
-using Cysharp.Threading.Tasks;
 using UnityScreenNavigator.Runtime.Core.Page;
 using UnityScreenNavigator.Runtime.Core.Modal;
-using UnityScreenNavigator.Runtime.Core.Sheet;
 using R3;   
 
 namespace SuiZin.InGame
@@ -9,7 +7,6 @@ namespace SuiZin.InGame
     public static class PlayerInputController
     {
         private static PageContainer _pageContainer;
-        private static ModalContainer _modalContainer;
 
         private static readonly ReactiveProperty<bool> _isInputable = new(true);
         public static Observable<bool> IsInputable => _isInputable;
@@ -18,16 +15,12 @@ namespace SuiZin.InGame
         public static void Initialize(PageContainer pageContainer,ModalContainer modalContainer)
         {
             _pageContainer = pageContainer;
-            _modalContainer = modalContainer;
+            _isInputable.Value = pageContainer.Pages.Count == 0;
 
-            Observable.Merge(
-                    Observable.EveryValueChanged(pageContainer, x => x.Pages.Count).Select(_ => Unit.Default),
-                    Observable.EveryValueChanged(modalContainer, y => y.Modals.Count).Select(_ => Unit.Default)
-                )
+            Observable.EveryValueChanged(pageContainer, x => x.Pages.Count)
                 .Subscribe(_ =>
                 {
-                    var inputable = pageContainer.Pages.Count == 0 && modalContainer.Modals.Count == 0;
-                    _isInputable.Value = inputable;
+                    _isInputable.Value = pageContainer.Pages.Count == 0;
                 });
 
 
